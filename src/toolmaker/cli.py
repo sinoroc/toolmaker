7989dfde148ac467523f1da1520de0d3c91f781f
sinoroc <sinoroc.code+git@gmail.com>
2019-06-10 18:59:59 +0200

    Move some code from 'cli' to 'core'

--- src/toolmaker/cli.py
+++ src/toolmaker/cli.py
@@ -50,6 +50,7 @@ def main():
     """
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)
+
     cwd_path = pathlib.Path.cwd()
     default_config_path = cwd_path.joinpath('toolmaker.cfg')
 
@@ -58,6 +59,7 @@ def main():
 
     config = None
     if args.config:
+        logger.info("Reading configuration from file '%s'", args.config)
         config = configparser.ConfigParser()
         try:
             config.read_file(args.config)
@@ -72,45 +74,7 @@ def main():
     if args.tools:
         tools_names = args.tools
 
-    logger.info("Preparing to build tools %s", tools_names)
-
-    venv_path = cwd_path.joinpath('venv')
-
-    logger.info("Creating virtual environment '%s'...", venv_path)
-    venv_context = core.venv_create(venv_path)
-
-    logger.info("Updating virtual environment")
-    core.venv_update(venv_context)
-
-    for tool_name in tools_names:
-        tool_config = config[tool_name]
-        if tool_name.endswith('.pex'):
-            logger.info("Building pex tool '%s'", tool_name)
-            core.build_pex(
-                cwd_path,
-                venv_context,
-                tool_name,
-                tool_config,
-                args.force,
-            )
-        if tool_name.endswith('.shiv'):
-            logger.info("Building shiv tool '%s'", tool_name)
-            core.build_shiv(
-                cwd_path,
-                venv_context,
-                tool_name,
-                tool_config,
-                args.force,
-            )
-        if tool_name.endswith('.zapp'):
-            logger.info("Building zapp tool '%s'", tool_name)
-            core.build_zapp(
-                cwd_path,
-                venv_context,
-                tool_name,
-                tool_config,
-                args.force,
-            )
+    core.build(cwd_path, config, tools_names, args.force)
 
 
 # EOF
--- src/toolmaker/core.py
+++ src/toolmaker/core.py
@@ -5,9 +5,14 @@
 """
 
 
+import logging
 import pathlib
 import subprocess
 import venv
+
+
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 def _run_in_venv(venv_context, command):
@@ -71,44 +76,93 @@ def _get_requirements(config):
     return requirements
 
 
-def _get_output_file_path(cwd_path, tool_name, config):
-    output_dir_path = cwd_path.joinpath(tool_name)
+def _get_output_file_path(work_dir_path, tool_name, config):
+    output_dir_path = work_dir_path.joinpath(tool_name)
     output_file_name = config['output_file']
     output_file_path = output_dir_path.joinpath(output_file_name)
     return output_file_path
 
 
-def build_pex(cwd_path, venv_context, tool_name, config, force):
+def build_pex(work_dir_path, venv_context, tool_name, config, force):
     """ Build pex
     """
-    output_file_path = _get_output_file_path(cwd_path, tool_name, config)
+    output_file_path = _get_output_file_path(work_dir_path, tool_name, config)
     if force or not output_file_path.exists():
         requirements = _get_requirements(config)
         entry_point = config['entry_point']
         output_file_path.parent.mkdir(exist_ok=True)
         _pex(venv_context, entry_point, output_file_path, requirements)
+    else:
+        LOGGER.info("Tool '%s' already exists, build skipped", tool_name)
 
 
-def build_shiv(cwd_path, venv_context, tool_name, config, force):
+def build_shiv(work_dir_path, venv_context, tool_name, config, force):
     """ Build shiv
     """
-    output_file_path = _get_output_file_path(cwd_path, tool_name, config)
+    output_file_path = _get_output_file_path(work_dir_path, tool_name, config)
     if force or not output_file_path.exists():
         requirements = _get_requirements(config)
         console_script = config['console_script']
         output_file_path.parent.mkdir(exist_ok=True)
         _shiv(venv_context, console_script, output_file_path, requirements)
+    else:
+        LOGGER.info("Tool '%s' already exists, build skipped", tool_name)
 
 
-def build_zapp(cwd_path, venv_context, tool_name, config, force):
+def build_zapp(work_dir_path, venv_context, tool_name, config, force):
     """ Build zapp
     """
-    output_file_path = _get_output_file_path(cwd_path, tool_name, config)
+    output_file_path = _get_output_file_path(work_dir_path, tool_name, config)
     if force or not output_file_path.exists():
         requirements = _get_requirements(config)
         entry_point = config['entry_point']
         output_file_path.parent.mkdir(exist_ok=True)
         _zapp(venv_context, output_file_path, entry_point, requirements)
+    else:
+        LOGGER.info("Tool '%s' already exists, build skipped", tool_name)
+
+
+def build(work_dir_path, config, tools_names, force):
+    """ Build tools
+    """
+    LOGGER.info("Preparing to build tools %s", tools_names)
+    venv_path = work_dir_path.joinpath('venv')
+
+    LOGGER.info("Creating virtual environment '%s'...", venv_path)
+    venv_context = _venv_create(venv_path)
+
+    LOGGER.info("Updating virtual environment")
+    _venv_update(venv_context)
+
+    for tool_name in tools_names:
+        tool_config = config[tool_name]
+        if tool_name.endswith('.pex'):
+            LOGGER.info("Building pex tool '%s'", tool_name)
+            build_pex(
+                work_dir_path,
+                venv_context,
+                tool_name,
+                tool_config,
+                force,
+            )
+        if tool_name.endswith('.shiv'):
+            LOGGER.info("Building shiv tool '%s'", tool_name)
+            build_shiv(
+                work_dir_path,
+                venv_context,
+                tool_name,
+                tool_config,
+                force,
+            )
+        if tool_name.endswith('.zapp'):
+            LOGGER.info("Building zapp tool '%s'", tool_name)
+            build_zapp(
+                work_dir_path,
+                venv_context,
+                tool_name,
+                tool_config,
+                force,
+            )
 
 
 class _EnvBuilder(venv.EnvBuilder):
@@ -122,7 +176,7 @@ class _EnvBuilder(venv.EnvBuilder):
         self.context = context
 
 
-def venv_create(venv_path):
+def _venv_create(venv_path):
     """ Create virtual environment
     """
     venv_builder = _EnvBuilder(with_pip=True)
@@ -130,7 +184,7 @@ def venv_create(venv_path):
     return venv_builder.context
 
 
-def venv_update(venv_context):
+def _venv_update(venv_context):
     """ Update virtual environment
     """
     _pip_install(venv_context, ['pip'])
